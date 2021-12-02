9080c039ca0aa88e5ad3fc842fe4ea7fb7d427ae
Baroly <a0961136519@gmail.com>
2021-12-02 14:13:55 +0800

    update02

--- src/main/java/com/d01/git/HelloGit.java
+++ src/main/java/com/d01/git/HelloGit.java
@@ -5,7 +5,13 @@ public class HelloGit {
 		System.out.println("Hello Git!");
 	}
 	
-	public static void show() {
+	public static void show01() {
 		System.out.println("aaa");
 	}
+	
+	public static void show02() {
+		System.out.println("aaa");
+	}
+	
+	
 }
