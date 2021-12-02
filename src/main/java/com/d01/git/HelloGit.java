2deddc8045981752647fa7ef5b8eab9269098de3
Baroly <a0961136519@gmail.com>
2021-12-02 16:31:13 +0800

    commit to master branch

--- src/main/java/com/d01/git/HelloGit.java
+++ src/main/java/com/d01/git/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("創建分支");
+		System.out.println("master commit");
 	}
 	
 	public static void show01() {
